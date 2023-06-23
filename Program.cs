131b086be4fdf6b4fa1c37c7862a752011c65c95
Fudge <fugenkenneth@gmail.com>
2023-06-23 17:05:58 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SpotifudgeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Track Track1 = new Track();
+
+            Track1.trackName = "COOL (Your Raindbow)";
+            Track1.artistName = "NMIXX";
+            Track1.lengthMin = 2;
+            Track1.lengthSec = 50;
+            Track1.albumName = "ENTWURF";
+            Track1.genre = "K-Pop";
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Discover Weekly");
+            Console.WriteLine(" ");
+            Console.WriteLine("Your weekly mixtape of fresh music. Enjoy new discoveries and deep cuts");
+            Console.WriteLine("picked just for you. Updates every Monday, so save your favorites!");
+            Console.WriteLine(" ");
+            Console.WriteLine("By Spotifudge");
+            Console.WriteLine(" ");
+            Console.WriteLine("1. " + Track1.trackName + " - " + Track1.artistName);
+        }
+    }
+}
---
+++ Track.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpotifudgeOOP
+{
+    class Track
+    {
+        public string trackName;
+        public string artistName;
+        public int lengthMin;
+        public int lengthSec;
+        public string albumName;
+        public string genre;
+
+        public void Play()
+        {
+
+        }
+
+        public void Pause()
+        {
+
+        }
+
+        public void Shuffle()
+        {
+
+        }
+
+        public void Repeat()
+        {
+
+        }
+
+        public void Skip()
+        {
+
+        }
+
+        public void Rewind()
+        {
+
+        }
+        
+        public void AddToPlaylist()
+        {
+
+        }
+
+        public void RemoveFromPlaylist()
+        {
+
+        }
+    }
+}
