85245738ae4f6e5caefe1ca167ec963c9547a15a
Fudge <fugenkenneth@gmail.com>
2023-06-23 21:29:48 +0800

    Added a "discoverWeekly" List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace SpotifudgeOOP
 {
@@ -7,7 +8,7 @@ namespace SpotifudgeOOP
         static void Main(string[] args)
         {
             Track Track1 = new Track();
-            Track1.trackName = "COOL (Your Raindbow)";
+            Track1.trackName = "COOL (Your Rainbow)";
             Track1.artistName = "NMIXX";
             Track1.lengthMin = 2;
             Track1.lengthSec = 50;
@@ -19,19 +20,26 @@ namespace SpotifudgeOOP
             Track2.artistName = "Kristiane";
             Track2.lengthMin = 2;
             Track2.lengthSec = 27;
-            Track2.albumName = "I Miss Mayself, Sometimes";
+            Track2.albumName = "I Miss Myself, Sometimes";
             Track2.genre = "Alternative/Indie";
 
-            Console.WriteLine(" ");
-            Console.WriteLine("Discover Weekly");
-            Console.WriteLine(" ");
-            Console.WriteLine("Your weekly mixtape of fresh music. Enjoy new discoveries and deep cuts");
-            Console.WriteLine("picked just for you. Updates every Monday, so save your favorites!");
-            Console.WriteLine(" ");
-            Console.WriteLine("By Spotifudge");
-            Console.WriteLine(" ");
+            Track Track3 = new Track();
+            Track3.trackName = "That's Hilarious";
+            Track3.artistName = "Charlie Puth";
+            Track3.lengthMin = 2;
+            Track3.lengthSec = 24;
+            Track3.albumName = "CHARLIE";
+            Track3.genre = "Pop";
+
+            var discoverWeekly = new List <string>()
+            {" ", "Discover Weekly", " ", "Your weekly mixtape of fresh music. Enjoy new discoveries and deep cuts", "picked just for you. Updates every Monday, so save your favorites!", " ", "By Spotifudge", " "};
+            foreach(string i in discoverWeekly)
+            {
+                Console.WriteLine(i);
+            }
             Console.WriteLine("1. " + Track1.trackName + " - " + Track1.artistName);
             Console.WriteLine("2. " + Track2.trackName + " - " + Track2.artistName);
+            Console.WriteLine("3. " + Track3.trackName + " - " + Track3.artistName);
         }
     }
 }
