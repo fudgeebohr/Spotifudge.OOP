af91f2a7d504564aedf06fac23d135173fcdd30b
Fudge <fugenkenneth@gmail.com>
2023-06-23 17:25:23 +0800

    Added Track 2

--- Program.cs
+++ Program.cs
@@ -7,13 +7,20 @@ namespace SpotifudgeOOP
         static void Main(string[] args)
         {
             Track Track1 = new Track();
-
             Track1.trackName = "COOL (Your Raindbow)";
             Track1.artistName = "NMIXX";
             Track1.lengthMin = 2;
             Track1.lengthSec = 50;
             Track1.albumName = "ENTWURF";
             Track1.genre = "K-Pop";
+
+            Track Track2 = new Track();
+            Track2.trackName = "Wish I Could Be Your Girl";
+            Track2.artistName = "Kristiane";
+            Track2.lengthMin = 2;
+            Track2.lengthSec = 27;
+            Track2.albumName = "I Miss Mayself, Sometimes";
+            Track2.genre = "Alternative/Indie";
 
             Console.WriteLine(" ");
             Console.WriteLine("Discover Weekly");
@@ -24,6 +31,7 @@ namespace SpotifudgeOOP
             Console.WriteLine("By Spotifudge");
             Console.WriteLine(" ");
             Console.WriteLine("1. " + Track1.trackName + " - " + Track1.artistName);
+            Console.WriteLine("2. " + Track2.trackName + " - " + Track2.artistName);
         }
     }
 }
