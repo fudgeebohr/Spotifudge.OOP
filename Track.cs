79dfe3f1b111d7920dcd6d1166f4dabd2199ba5f
Fudge <fugenkenneth@gmail.com>
2023-07-01 21:00:52 +0800

    Added get and set accessors

--- Track.cs
+++ Track.cs
@@ -1,26 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SpotifudgeOOP
 {
     class Track
     {
-        public string trackName;
-        public string artistName;
-        public int lengthMin;
-        public int lengthSec;
-        public string albumName;
-        public string genre;
+        public string trackName { get; set; }
+        public string artistName { get; set; }
+        public int lengthMin { get; set; }
+        public int lengthSec { get; set; }
+        public string albumName { get; set; }
+        public string genre { get; set; }
+
+        private bool isPlaying;
+        private bool isPaused;
+        private bool isRepeated;
 
         public void Play()
         {
-
+            if (!isPlaying)
+            {
+                Console.WriteLine("Playing: " + trackName);
+                isPlaying = true;
+                isPaused = false;
+            }
+            else
+            {
+                Console.WriteLine("Track is already playing.");
+            }
         }
 
         public void Pause()
         {
-
+            if (isPlaying && !isPaused)
+            {
+                Console.WriteLine("Pausing: " + trackName);
+                isPaused = true;
+            }
+            else if (!isPlaying)
+            {
+                Console.WriteLine("Cannot pause. Track is not currently playing.");
+            }
+            else
+            {
+                Console.WriteLine("Track is already paused.");
+            }
         }
 
         public void Shuffle()
@@ -30,7 +57,15 @@ namespace SpotifudgeOOP
 
         public void Repeat()
         {
-
+            if (!isRepeated)
+            {
+                Console.WriteLine("Repeating: " + trackName);
+                isRepeated = true;
+            }
+            else
+            {
+                Console.WriteLine("Track is already set to repeat.");
+            }
         }
 
         public void Skip()
